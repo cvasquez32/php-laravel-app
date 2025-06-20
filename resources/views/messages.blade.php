9161b259444e7cf041d3f16e4a3218790e672406
carlos vasquez <cvasquez0100@gmail.com>
2025-06-19 20:44:05 -0500

    add timezone for central time

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -6,7 +6,7 @@
     <div class="grid gap-6 mt-6">
       @forelse ($messages as $message)
       <div class="bg-white text-gray-900 p-6 rounded-lg shadow">
-      <h5 class="text-sm mb-2">Sent on: {{ $message->created_at->format('F j, Y g:i A')}}</h5>
+      <h5 class="text-sm mb-2">Sent on: {{ $message->created_at->timezone('America/Chicago')->format('F j, Y g:i A')}}</h5>
         <h2 class="text-lg font-semibold mb-1">Sender: {{ $message->sender_name }}</h2>
         <h3 class="text-sm text-indigo-600 mb-2">Email: {{ $message->sender_email }}</h3>
         <p class="text-sm text-gray-800">{{ $message->message }}</p>
